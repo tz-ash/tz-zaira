619fc5a96965686ebf122e220ea817fcf5fbc972
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-07-28 19:38:38 +0530

    Update blacklist_stickers.py

--- MashaRoBot/modules/blacklist_stickers.py
+++ MashaRoBot/modules/blacklist_stickers.py
@@ -505,9 +505,19 @@ Blacklist sticker is used to stop certain stickers. Whenever a sticker is sent, 
  ❍ /blstickermode <ban/tban/mute/tmute>*:* sets up a default action on what to do if users use blacklisted stickers
 Note:
  ❍ <sticker link> can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
+Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
+
+*NOTE*: Blacklists do not affect group admins.
+
+ ❍ /blacklist*:* View the current blacklisted words.
+
+Admin only:
+ ❍ /addblacklist <triggers>*:* Add a trigger to the blacklist. Each line is considered one trigger, so using different lines will allow you to add multiple triggers.
+ ❍ /unblacklist <triggers>*:* Remove triggers from the blacklist. Same newline logic applies here, so you can remove multiple triggers at once.
+ ❍ /blacklistmode <off/del/warn/ban/kick/mute/tban/tmute>*:* Action to perform when someone sends blacklisted words.
 """
 
-__mod_name__ = "B STICK"
+__mod_name__ = "B-LIST/STICK"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True
