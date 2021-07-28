fc0aa814ad1a2754ec21227fe89854e302c87882
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-07-29 04:56:01 +0530

    Update __google.py

--- MashaRoBot/modules/__google.py
+++ MashaRoBot/modules/__google.py
@@ -267,7 +267,7 @@ async def apk(e):
             + app_link
             + "'>View in Play Store</a>"
         )
-        app_details += "\n\n===> Masha <==="
+        app_details += "\n\n===> ZAIRA <==="
         await e.reply(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
         await e.reply("No result found in search. Please enter **Valid app name**")
@@ -275,13 +275,13 @@ async def apk(e):
         await e.reply("Exception Occured:- " + str(err))
 
 
-__mod_name__ = "GOOGLE"
+__mod_name__ = "SEAECH"
 
 __help__ = """
  ❍ /google <text>*:* Perform a google search
  ❍ /img <text>*:* Search Google for images and returns them\nFor greater no. of results specify lim, For eg: `/img hello lim=10`
  ❍ /app <appname>*:* Searches for an app in Play Store and returns its details.
  ❍ /reverse: Does a reverse image search of the media which it was replied to.
- ❍ Masha <query>*:* Masha answers the query
-  💡Ex: `Masha where is India?`
+ ❍ ZAIRA <query>*:* ZAIRA answers the query
+  💡Ex: `ZAIRA where is India?`
 """
