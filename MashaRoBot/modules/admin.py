97e211196c9922787dd1f366f829064d3cfcfa1c
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-07-28 21:53:47 +0530

    Update admin.py

--- MashaRoBot/modules/admin.py
+++ MashaRoBot/modules/admin.py
@@ -656,6 +656,14 @@ and Automatically openned at 6 am(IST) To Prevent Night Spams.
 *Admins only:*
  ❍ /setrules <your rules here>*:* set the rules for this chat.
  ❍ /clearrules*:* clear the rules for this chat.
+
+*GROUPS EDITINGBONLY FOR GROUP ADMINS*
+
+ ❍ /setgtitle <newtitle>*:* Sets new chat title in your group.
+ ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
+ ❍ /delgpic*:* Same as above but to remove group profile pic.
+ ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
+ ❍ /setdescription <description>*:* Sets new chat description in group.
 """
 
 ADMINLIST_HANDLER = DisableAbleCommandHandler("admins", adminlist)
